e99c137f567f19114355b6720ab1fc8233bb6ad1
Liran Tal <liran.tal@gmail.com>
2025-02-17 12:45:13 +0200

    fix: update for supported model and env variable API
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- server/api/analyze.post.ts
+++ server/api/analyze.post.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
     const { image } = body;
 
     const response = await openai.chat.completions.create({
-      model: "gpt-4-vision-preview",
+      model: "gpt-4o-mini",
       messages: [
         {
           role: "user",
@@ -23,12 +23,15 @@ export default defineEventHandler(async (event) => {
             },
             {
               type: "image_url",
-              image_url: image
+              image_url: {
+                "url": image
+              }, 
             }
           ],
         },
       ],
-      max_tokens: 1
+      max_tokens: 1,
+      temperature: 0,
     });
 
     const result = response.choices[0].message.content;
